bade8ae42ac76576a11c4f635186095cfe1e2fc3
swings <silverwings999@gmail.com>
2013-09-09 15:38:03 +0800

    1. 資料夾結構修改

---
+++ Client/Assets/Editor/Tools.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+using SmoothMoves;
+/// <summary>
+/// 中文
+/// </summary>
+public static class Tools {
+
+    [MenuItem("Tools/RemoveWhite")]
+    public static void RemoveWhite()
+    {
+        if (Selection.activeObject is Texture2D)
+        {
+            Texture2D t2d = Selection.activeObject as Texture2D;
+            TextureImporter ti = TextureImporter.GetAtPath(AssetDatabase.GetAssetPath(t2d)) as TextureImporter;
+            if (ti)
+            {
+                ti.isReadable = true;
+                AssetDatabase.ImportAsset(AssetDatabase.GetAssetPath(t2d));
+            }
+            Color[] colors = t2d.GetPixels();
+            for (int i = 0; i < colors.Length; i++)
+                if (colors[i].grayscale == 1)
+                    colors[i].a = 0;
+            t2d.SetPixels(colors);
+        }
+    }
+	[MenuItem("Tools/RandomTest")]
+	public static void RandomTest()
+	{
+		int count1 = 0;
+		int count2 = 0;
+		float Rate1 = 0.5f;
+		float Rate2 = 0.5f;
+		for (int i = 0; i < 10000; i++)
+		{
+			float r = Random.value;
+			if (r >= 0 && r < Rate1)
+			{
+				count1++;
+				continue;
+			}
+			r = Random.value;
+			if (r >= 0 && r < Rate2)
+				count2++;
+			r = Random.value;
+		}
+		Debug.Log(count1);
+		Debug.Log(count2);
+
+		count1 = 0;
+		count2 = 0;
+		float Rate3 = Rate2 / (1f - Rate1);
+		for (int i = 0; i < 10000; i++)
+		{
+			float r = Random.value;
+			if (r >= 0 && r < Rate1)
+			{
+				count1++;
+				continue;
+			}
+			r = Random.value;
+			if (r >= 0 && r < Rate3)
+				count2++;
+			r = Random.value;
+		}
+		Debug.Log(count1);
+		Debug.Log(count2);
+	}
+
+	[MenuItem("Tools/QuickTest")]
+	public static void QuickTest()
+	{
+		Texture2D t2d = new Texture2D(256,256);
+		Vector2 center=  new Vector2(128,128);
+		for(int i = 0 ; i < 129 ;i++)
+			for(int j = 0 ; j < 129 ;j++)
+			{
+				Color c =Color.clear;
+				float d = Vector2.Distance(new Vector2(i,j),center);
+				if(d < 100)
+				{
+					c.a =Mathf.Clamp01((100 - d + 50) / 100f);
+				}
+				t2d.SetPixel(i,j,c);
+				t2d.SetPixel(256-i,256-j,c);
+				t2d.SetPixel(i,256-j,c);
+				t2d.SetPixel(256-i,j,c);
+			}
+		t2d.Apply();
+		AssetDatabase.CreateAsset(t2d, AssetDatabase.GenerateUniqueAssetPath("Assets/TestShadow.Asset"));
+
+	}
+	[MenuItem("Tools/QuickTest1")]
+	public static void QuickTest1()
+	{
+
+		if (Selection.activeGameObject && Selection.activeGameObject.GetComponent<ParticleSystem>())
+		{
+			ParticleSystem ps = Selection.activeGameObject.GetComponent<ParticleSystem>();
+			//Particle p = ps.
+			
+			ParticleSystem.Particle[] pss = new ParticleSystem.Particle[0];
+			Debug.Log(ps.particleCount);
+		}	
+	}
+}
---
+++ Client/Assets/Script/TestScript/SimpleExit.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+[ExecuteInEditMode]
+public class SimpleExit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public bool DisplayExit = false;
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			DisplayExit = true;
+		}
+	}
+	public Rect BoxRect = new Rect(0.3f, 0.3f, 0.4f, 0.4f);
+	public Rect LabelRect = new Rect(0.1f, 0.1f, 0.8f, 0.4f);
+	public Rect ButtonRect = new Rect(0.1f, 0.6f, 0.3f, 0.3f);
+	void OnGUI()
+	{
+		if (DisplayExit)
+		{
+			Rect ScreeRect = new Rect(Screen.width * BoxRect.x, Screen.height * BoxRect.y, Screen.width * BoxRect.width, Screen.height * BoxRect.height);
+			GUI.Box(ScreeRect, "");
+			GUI.BeginGroup(ScreeRect);
+			Rect tempRect = LabelRect;
+			tempRect.x *= ScreeRect.width;
+			tempRect.y *= ScreeRect.height;
+			tempRect.width *= ScreeRect.width;
+			tempRect.height *= ScreeRect.height;
+
+			GUI.skin.label.alignment = TextAnchor.MiddleCenter;
+			GUI.Label(tempRect, "Exit");
+
+			tempRect = ButtonRect;
+			tempRect.x *= ScreeRect.width;
+			tempRect.y *= ScreeRect.height;
+			tempRect.width *= ScreeRect.width;
+			tempRect.height *= ScreeRect.height;
+			if (GUI.Button(tempRect, "Yes"))
+				Application.Quit();
+			tempRect.x = ScreeRect.width * (1 - (ButtonRect.width + ButtonRect.x));
+			if(GUI.Button(tempRect, "No"))
+				DisplayExit = false;
+			GUI.EndGroup();
+		}
+	}
+}
